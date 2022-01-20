2395108e38ffd5a63fdd492e9d5e507505785d05
Lindsay <lbtannahill@gmail.com>
2022-01-19 17:17:58 -0800

    refactored and added backwards compatibilities

--- wordsearch.js
+++ wordsearch.js
@@ -1,26 +1,24 @@
 const transpose = require('./transpose');
 
 const wordSearch = (letters, word) => {
-  let wordFound = false;
 
   const looper = function(input) {
-    for (letter of input) {
-      if (letter.includes(word)) {
-        wordFound = true;
+    for (let letter of input) {
+      if (letter.includes(word) || letter.includes(word.split('').reverse().join(''))) {
+        return true;
       }
     }
+    return false;
   };
 
   const horizontalJoin = letters.map(ls => ls.join(''));
-  looper(horizontalJoin);
- 
   let transposed = transpose(letters);
-
   let verticalJoin = transposed.map(ls => ls.join(''));
-  looper(verticalJoin);
-
-  return wordFound;
+  
+  return looper(horizontalJoin) || looper(verticalJoin) || false;
 };
+
+
 
 
 module.exports = wordSearch;
